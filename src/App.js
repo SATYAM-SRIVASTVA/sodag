22f2c5db6a59d2dbd7bb4ccac2633cd63b2e0e76
adityakumar-blip <adityakamal303@gmail.com>
2022-04-25 09:28:44 +0530

    Routing Completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter as Router,Routes ,Switch ,Route, BrowserRouter } from 'react-router-dom';
 
 import Header from './Components/Header';
 import Navbar from './Components/Navbar';
-import Featured from './Components/Featured';
-import Textimg from './Components/Textimg';
-import Footer from './Components/Footer';
+import Acidity from './Components/Acidity';
+
+
 function App() {
   return (
     <>
+
+    <Router>
     <Navbar/>
-    <Header/>
-    <Featured/>
-    <Textimg/>
-    <Footer/>
+    <Routes>
+      <Route path='/' exact element={<Header/>} />
+      <Route path='/acidity' element={<Acidity/>} />
+    </Routes>
+
+    </Router>
+    
 
 
     </>
